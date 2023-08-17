b7f60b070bdb7da39f578511b1c7d4d8e24eda0e
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-08-16 20:51:59 -0400

    Config GeoCoding API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,57 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const savedCity = localStorage.getItem("savedCity") || "";
+  const [city, setCity] = useState(savedCity.replace('"', ""));
+
+  useEffect(() => {
+    localStorage.setItem("savedCity", city);
+
+    // Sistema api clima
+  }, [city]);
+
+  const [coordinates, setCoordinates] = useState(any);
+  const [weather, setWeather] = useState(any);
+
+  const API_KEY = "4e210f2d8d38eb0d142a7352ef4c5c80";
+  const geocoding_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit={limit}&appid=${API_KEY}`;
+
+  const geocodeAddress = async (adress: string) => {
+    try {
+      const response = await axios.get(geocoding_URL, {
+        params: {
+          adress: adress,
+          key: API_KEY,
+        },
+      });
+
+      return response.data.results;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    const getAdress = async () => {
+      try {
+        const results = await geocodeAddress(city);
+        if (results.length > 0) {
+          const { lat, lng } = results[0].geometry.location;
+          setCoordinates({ latitude: lat, longitude: lng });
+        }
+      } catch (error) {
+        console.error("Erro ao geocodificar o endereço:", error);
+      }
+    };
+
+    getAdress();
+  });
+
+  const openWeather_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${API_KEY}`;
+
   return (
     <main className="flex h-screen flex-col justify-center">
       <div className="flex flex-row justify-around">
@@ -12,6 +63,8 @@ export default function Home() {
               placeholder="Digite a cidade aqui"
               name="city"
               id="city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
             />
             <h3 className="font-medium">21 de Agosto, Segunda-feira</h3>
           </div>
